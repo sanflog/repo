0a9b7ff2ad22220c357baa1b8e5cc261f123a176
sanflog <daichan613@gmail.com>
2022-04-23 21:52:56 +0900

    practice

---
+++ documents_php/admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Administration | Documents</title>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Language" value="ja-JP" />
+        <meta name="viewport" content="width=device-width, initial-scale-1.0" />
+        <link rel="stylesheet" type="text/css" href="./main.css" />
+    </head>
+    <body>
+        <div class="form">
+            <h3>title</h3>
+            <h3>author</h3>
+            <h3>publish date</h3>
+            <h3>category</h3>
+            <h3>body</h3>
+        </div>
+    </body>
+</html>
---
+++ documents_php/test.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>test php</h1>
+        <?php echo '<p>Hello World</p>'; ?>
+    </body>
+</html>
